fab3ef2a6c1aa23d00f4a024a71cd9917e4c29e3
KhizerUrRehman <35479361+KhizerUrRehman@users.noreply.github.com>
2024-12-14 09:57:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React wow grape
+          Learn React wow grape grape
         </a>
       </header>
     </div>
